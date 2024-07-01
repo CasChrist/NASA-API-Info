36133a7c3621112d06bf6ca280f705c49c246126
CasChrist <sozurukovmaksim@gmail.com>
2024-07-02 04:53:13 +0700

    implement FLR

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,25 @@
-import { fetchNASAData } from './api.js';
-import { processNASAData } from './data.js';
-import { renderNASAData } from './display.js';
+import { fetchNASAData, fetchFLRData } from './api.js';
+import { processNASAData, processFLRData } from './data.js';
+import { renderNASAData, renderFLRData } from './display.js';
 
-// Функция для инициализации приложения
+// Функция для инициализации метода Image of The Day
 function chooseDate() {
     const nasaContainer = document.getElementById('nasa-container');
     nasaContainer.innerHTML = `
     <p>Enter the desired date (format YYYY-MM-DD):</p><br>
     <input id="date"></input>
     <button id="temp">Submit</button>`
-    document.getElementById('temp').onclick = imgDay;
+    try {
+        document.getElementById('temp').onclick = imgDay;
+    } catch {
+        console.log('mismatch');
+    }
+
+    try {
+        document.getElementById('temp').onclick = FLR;
+    } catch {
+        console.log('mismatch');
+    }
 }
 
 async function imgDay() {
@@ -27,6 +37,21 @@ async function imgDay() {
     }
 }
 
-export { chooseDate, imgDay };
+async function FLR() {
+    const date = document.getElementById('date').value;
+    const FLRData = await fetchFLRData(date);
 
-console.log('script main.js loaded')
+    if (FLRData) {
+        const processedData = processFLRData(FLRData);
+        renderFLRData(processedData);
+    } else {
+        // Если данные не удалось получить, отобразим соответствующее сообщение на странице
+        const errorMessage = 'Failed to fetch NASA data. Please try again later.';
+        const nasaContainer = document.getElementById('nasa-container');
+        nasaContainer.innerHTML = `<p>${errorMessage}</p>`;
+    }
+}
+
+export { chooseDate, imgDay, FLR };
+
+console.log('script main.js loaded');
