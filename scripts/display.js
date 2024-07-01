02a627669f590f0a2f34651effa0185c5a56999b
CasChrist <sozurukovmaksim@gmail.com>
2024-07-02 01:38:45 +0700

    complete Image of The Day

--- scripts/display.js
+++ scripts/display.js
@@ -2,10 +2,12 @@
 function renderNASAData(nasaData) {
   const nasaContainer = document.getElementById('nasa-container');
   nasaContainer.innerHTML = `
+    <article>
       <h2>${nasaData.title}</h2>
-      <p>Date: ${nasaData.date}</p>
-      <img src="${nasaData.imageURL}" alt="NASA Image of the Day">
+      <time>Date: ${nasaData.date}</time>
       <p>${nasaData.explanation}</p>
+      <img src="${nasaData.imageURL}" alt="NASA Image of the Day">
+    </article>
   `;
 }
 
