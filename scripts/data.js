cc331425a7376c727039a670d08f5f4704f3ebf3
CasChrist <sozurukovmaksim@gmail.com>
2024-07-02 04:28:05 +0700

    implement processFLRData

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,5 @@
 // Функция для обработки полученных данных о космической фотографии дня
 function processNASAData(data) {
-  // В этой функции можно осуществлять любую необходимую обработку данных
-  // Например, фильтрация, сортировка или выделение нужных полей
 
   return {
       title: data.title,
@@ -11,4 +9,27 @@ function processNASAData(data) {
   };
 }
 
-export { processNASAData };
+function processFLRData(data) {
+  solar_flare = data;
+  
+  // Извлекаем нужную информацию
+  let info = [
+    "FLR ID: " + solarFlare.flrID,
+    "Начало: " + solarFlare.beginTime,
+    "Пик: " + solarFlare.peakTime,
+    "Тип класса: " + solarFlare.classType,
+    "Регион на солнце: " + solarFlare.sourceLocation,
+    "Примечание: " + solarFlare.note,
+    "Ссылка на подробности: <a href='" + solarFlare.link + "' target='_blank'>" + solarFlare.link + "</a>",
+    "Связанные события:"
+  ];
+
+  // Добавляем связанные события в список
+  solarFlare.linkedEvents.forEach(function(event) {
+    info.push("- " + event.activityID);
+  });
+
+  return info;
+}
+
+export { processNASAData, processFLRData };
