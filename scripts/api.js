8350879b14721be7c395c0f75bda567eb5c56a81
CasChrist <sozurukovmaksim@gmail.com>
2024-07-02 04:17:43 +0700

    implement fetchFLRData

--- scripts/api.js
+++ scripts/api.js
@@ -3,7 +3,6 @@ import { API_KEY } from "./api_key.js";
 // Функция для выполнения запроса к API NASA и получения данных о космических фотографиях
 async function fetchNASAData(date) {
     try {
-        document.getElementById
         const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${API_KEY}`);
         const data = await response.json();
         return data; // Возвращаем объект с данными о космической фотографии дня
@@ -13,4 +12,16 @@ async function fetchNASAData(date) {
     }
 }
 
-export { fetchNASAData };
+// Функция для выполнения запроса к API NASA и получения данных о солнечном всплеске
+async function fetchFLRData(date) {
+    try {
+        const response = await fetch(`https://api.nasa.gov/DONKI/FLR?startDate=${date}&api_key=${API_KEY}`);
+        const data = await response.json();
+        return data; // Возвращаем объект с данными о солнечном всплеске
+    } catch (error) {
+        console.error('Eror fetching FLR data:', error);
+        return null; // Возвращаем null в случае ошибки
+    }
+}
+
+export { fetchNASAData, fetchFLRData };
